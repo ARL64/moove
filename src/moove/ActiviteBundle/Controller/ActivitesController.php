bfcdc1e519fc0d44b01b3437f557062aa2d39da4
ARL64 <fabien.dartigues@outlook.fr>
2016-01-14 19:07:18 +0000

    Ajout d'assets / Héritage du bundle FOSUser sur ActiviteBundle / Ajout de vues

--- src/moove/ActiviteBundle/Controller/ActivitesController.php
+++ src/moove/ActiviteBundle/Controller/ActivitesController.php
@@ -6,7 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ActivitesController extends Controller
 {
+    // /!\ Actions métier
     
+    /**
+     * Renvoie à la page "http://moove-arl64.c9users.io/web/app_dev.php/accueil"
+     * 
+     */
     public function tableauDeBordAction()
     {
         return $this->render('mooveActiviteBundle:Accueil:tableauDeBordAccueil.html.twig');
@@ -14,28 +19,64 @@ class ActivitesController extends Controller
     
     public function detailsActiviteAction($idActivite)
     {
+        /** Repository de Participer */
+        $repParticiper = $this->getDoctrine()->getManager()->getRepository('mooveActiviteBundle:Participer');
+        /** Repository de Activite */
         $repActivite = $this->getDoctrine()->getManager()->getRepository('mooveActiviteBundle:Activite');
+        /** Repository de Utilisateur */
+        $repUtilisateur = $this->getDoctrine()->getManager()->getRepository('mooveActiviteBundle:Utilisateur');
+        // On récupère l'activité par l'id de l'activite $idActivite
         $activite = $repActivite->find($idActivite);
-        return $this->render('mooveActiviteBundle:Activite:detailsActivite.html.twig', array('activite' => $activite));
+        // On récupère un tableau d'objet Participer $tabParticiper
+        $tabParticiper = $repParticiper->findByIdActivite($idActivite);
+        // On initialise le tableau des objets Utilisateur
+        $tabParticipants = [];
+        // Pour chaque objet Participer on récupère l'utilisateur dans $tabParticipants
+        foreach ($tabParticiper as $participer) {
+            // On met dans le tableau des participants les utilisateurs
+            $tabParticipants[] = $repUtilisateur->find($participer->getIdUtilisateur());
+        }
+        return $this->render('mooveActiviteBundle:Activite:detailsActivite.html.twig', array('activite' => $activite, 'tabParticipants' => $tabParticipants));
     }
     
+    /**
+     * Renvoie à la page "http://moove-arl64.c9users.io/web/app_dev.php/historique"
+     * 
+     * @param $page (integer) numéro de la page actuel 
+     */
     public function historiqueAction($page)
-    {   // Liste des activitées finies
+    {   
+        /** Liste des activitées finies, 20 résultats par page */   
         $tabActivites = $this->getListeParticipationForUser(true, $page, 20);
+        /** Liste du nombre de participations */
         $tabNbParticipants = $this->getNbParticipantsParActivite($tabActivites);
         return $this->render('mooveActiviteBundle:Accueil:tableauDeBordHistorique.html.twig',
                             array('tabActivites' => $tabActivites, 'tabNbParticipants' => $tabNbParticipants));
     }
     
+    /**
+     * Renvoie à la page "http://moove-arl64.c9users.io/web/app_dev.php/activites/{page}"
+     * 
+     * @param $page (integer) numéro de la page actuel 
+     */
     public function enCoursAction($page)
-    {   // Liste des activitées en approche
-        $tabActivites = $this->getListeParticipationForUser(false, $page, 20);
+    {   
+        /** liste des activités de l'utilisateur courrant*/
+        $tabActivites = $this->getListeParticipationForUser(false, $page, 20); 
+        /** liste du nombre de participant pour chaque activité en corélation avec $tabActivite */
         $tabNbParticipants = $this->getNbParticipantsParActivite($tabActivites);
         return $this->render('mooveActiviteBundle:Accueil:tableauDeBordActivites.html.twig', 
                             array('tabActivites' => $tabActivites, 'tabNbParticipants' => $tabNbParticipants));
     }
+    // /!\ Fin actions métier
     
-    
+    // /!\ Fonction à partir d'ici 
+    /**
+     * Retourne une liste du nombre de participant pour chaque activité fournis dans la $tabActivites.
+     * 
+     * @param $tabActivites (array<Activite>) Tableau contenant des activités
+     * @return array<Integer>
+     */
     protected function getNbParticipantsParActivite($tabActivites)
     {
         // Vérifie si l'utilisateur est authentifier 
@@ -44,35 +85,44 @@ class ActivitesController extends Controller
             throw $this->createAccessDeniedException(); 
         }
         
-        // On prend le manager & le dossier des Participants
+        /** Repository de Participer */
         $repParticiper = $this->getDoctrine()->getManager()->getRepository('mooveActiviteBundle:Participer');
 
-		// on récupère la liste des participants, et on la convertie en quantité
+		/** Liste du nombre de participants */
         $listeNbParticipant = array();
 	    foreach ($tabActivites as $activite) 
 	    {
 	        $listeParticipant = $repParticiper->findByIdActivite($activite->getId());
             $listeNbParticipant[] = count($listeParticipant);
         }
+        
         return $listeNbParticipant;
     }
     
+    /**
+     * Retourne la liste des Activité auquel participe l'utilisateur actuellement autentifié.
+     * 
+     * @param $terminer (boolean) indique si les activités son finis (true) ou non (false) 
+     * @param $page (integer) indique la page actuel de recherche (fonctionn de pair avec $nbResultatPage)
+     * @param $nbResultatPage (integer) indique le nombre de résultat a afficher par $page
+     * @return array<Activite>
+     */
     protected function getListeParticipationForUser($terminer, $page, $nbResultatPage)
     {
-        // Vérifie si l'utilisateur est authentifier 
+        // Vérifie si l'utilisateur est authentifié 
         if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) 
         { 
             throw $this->createAccessDeniedException(); 
         }
-        // On obtien l'utilisateur courrant
+        // On obtient l'utilisateur courrant
         $utilisateur = $this->getUser(); 
 
-
-        // On prend le manager & le dossier des Participants
+        // On récupère le manager & le repository des Participants : Participer
 		$repParticiper = $this->getDoctrine()->getManager()->getRepository('mooveActiviteBundle:Participer');
+		// On récupère le manager & le repository des Activités : Activite
 		$repActivite = $this->getDoctrine()->getManager()->getRepository('mooveActiviteBundle:Activite');
 
-		// On récupère la liste des Participation liés à cette personne
+		// On récupère la liste des Participations liés à la personne connectée $utilisateur
 		$listeEntiereParticiper = $repParticiper->findBy(array('idUtilisateur' => $utilisateur));
 		
 		// optimisable
@@ -83,9 +133,14 @@ class ActivitesController extends Controller
             $listeIdActivite[] = $participe->getIdActivite();
         }
 
+        // on retourne la liste des activités, terminées ou non ($terminer), triées par ordre croissant selon la date,
+        // avec une limite de $nbResultatPage activités
 		return $repActivite->findBy(array('id'=>$listeIdActivite, 'estTerminee' => $terminer), 
 		                            array('dateHeureRDV'=>'ASC'), 
 		                            $nbResultatPage, 
 		                            ($page-1)*$nbResultatPage);
     }
-}
+    
+    
+    
+} // fin de "class ActivitesController extends Controller"
